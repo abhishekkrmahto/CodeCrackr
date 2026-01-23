037d99e9d214f335074e72bad27161d40956b856
avishekkrmahto <abhishek.mahto2025@gmail.com>
2026-01-23 15:23:43 +0530

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LandingPage from "./components/LandingPage/LandingPage"
+import '../src/App.css'
+import HomePage from "./components/Homepage/HomePage"
 
 const App = () => {
   return (
-    <div>
+   <>
+    <BrowserRouter>
+    <Routes>
+
       
-    </div>
+      <Route path="/" element={<LandingPage></LandingPage>}></Route>
+      <Route path="/home" element={<HomePage></HomePage>}></Route>
+
+
+    </Routes>
+    </BrowserRouter>
+    
+    </>
   )
 }
 
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "../LandingPage/LandingPage.css";
+import { useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className="container flex justify-center items-center w-screen h-screen text-white">
+        <div className="insideBox border-4 rounded-2xl p-15 justify-center items-center flex flex-col gap-10">
+          <div className="top1 rounded-2xl cursor-pointer border p-4 font-bold">
+            <h1>AI Powered Interview Preparation</h1>
+          </div>
+
+          <div className="top2 p-2 flex flex-col gap-5">
+            <h1 className="top2-text text-5xl tracking-widest font-extrabold">
+              ACE YOUR NEXT
+            </h1>
+            <h1 className="top2-text text-5xl tracking-widest font-extrabold">
+              TECH INTERVIEW
+            </h1>
+          </div>
+
+          <div className="top3  p-3">
+            <p className="w-[600px] text-center text-xl ">
+              Practice with role specific questions,get ai powered feedback and
+              result and,track your progress prepare smarter,not harder
+            </p>
+          </div>
+
+          <div
+            className="top4 rounded-2xl font-bold cursor-pointer bg-teal-700"
+          >
+            <div className="get-started-with-google-button p-3" onClick={(e)=>{navigate('/home')}}>
+              <h1>Get Started With Google</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
